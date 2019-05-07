dbbc02ecc348b4acdd17659cb9f8a88fb3c4efd7
KonradMysz <50356115+KonradMysz@users.noreply.github.com>
2019-05-07 17:38:04 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,10 @@
+package bootcamp;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println(1 + 1);
+        System.out.println(1900 + 49 - 1389 + 8 * 9);
+    }
+}
